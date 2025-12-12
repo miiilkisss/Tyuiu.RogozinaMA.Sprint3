878681b072cc0ff538de595d83efaa8e5947e2d2
mila.rogo@yandex.ru <mila.rogo@yandex.ru>
2025-12-12 16:16:43 +0500

    Task0

--- Tyuiu.RogozinaMA.Sprint3.Task1.V1.Lib/DataService.cs
+++ Tyuiu.RogozinaMA.Sprint3.Task1.V1.Lib/DataService.cs
@@ -3,21 +3,21 @@ using tyuiu.cources.programming.interfaces.Sprint3;
 
 namespace Tyuiu.RogozinaMA.Sprint3.Task1.V1.Lib
 {
-    public class DataService: ISprint3Task1V1
+    public class DataService : ISprint3Task1V1
     {
-            public double GetMultiply()
+        public double GetMultiply()
+        {
+            double product = 1.0;
+            int k = 1;
+
+            while (k <= 7)
             {
-                double product = 1.0;
-                int k = 1;
-
-                while (k <= 7)
-                {
-                    double term = Math.Pow(3.0 / k, -2);
-                    product *= term;
-                    k++;
-                }
-
-                return Math.Round(product, 3);
+                double term = Math.Pow(3.0 / k, -2);
+                product *= term;
+                k++;
             }
+
+            return Math.Round(product, 3);
         }
     }
+}
