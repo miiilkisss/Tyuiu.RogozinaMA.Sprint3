6384a2fe7b9a21da910da373c69770fb5c4aea51
mila.rogo@yandex.ru <mila.rogo@yandex.ru>
2025-12-12 16:52:58 +0500

    Task3

--- Tyuiu.RogozinaMA.Sprint3.Task3.V19.Lib/DataService.cs
+++ Tyuiu.RogozinaMA.Sprint3.Task3.V19.Lib/DataService.cs
@@ -7,12 +7,12 @@ namespace Tyuiu.RogozinaMA.Sprint3.Task3.V19.Lib
     {
         public string ReplaceCharOnNum(string value, char replaceable, char replacement)
         {
-            // Используем цикл foreach для замены символов
+            
             string result = "";
 
             foreach (char ch in value)
             {
-                // Если символ равен replaceable, заменяем на replacement
+                
                 if (ch == replaceable)
                 {
                     result += replacement;
